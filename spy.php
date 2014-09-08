81cc7a4d4c29994a97c9619d7729e2b94444304e
Xavier Mertens <xavier@rootshell.be>
2014-09-08 22:28:27 +0200

    Cleanup code

--- spy.php
+++ spy.php
@@ -4,4 +4,4 @@
 // require_once 'apps/superlog/spy.php';
 require_once 'apps/eslog/lib/log.php';
 require_once 'apps/eslog/lib/hooks.php';
-OC_SuperLog_Hooks::all($_SERVER);
+OC_esLog_Hooks::all($_SERVER);
